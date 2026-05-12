4dfca9a81fe0de86cc6bf70a5fc1a32fd6b3d913
GabbyViernes <viernes.gabriellealbert310@gmail.com>
2026-05-13 07:42:29 +0800

    fix: force install production dependencies in build script

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Install whatever is in the text file
 pip install -r requirements.txt
+
+# FORCE install the Render production dependencies
+pip install dj-database-url whitenoise "uvicorn[standard]"
+
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
